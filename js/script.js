c987558acb93059745b4cea1377afe85b4a773d7
Julian <julianricardo423@gmail.com>
2024-02-09 09:33:32 -0600

    Se agrega comentarios al código

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 let procesoFactorial = () =>
 {
     let numeroFactorial = 1;
+    //Conversión a número
     let numeroI = Number.parseInt(document.getElementById("numero").value);
+
+    //Variable para escribir el resultado final
     let resultadoFinal = document.getElementById('resultado');
+    //Se valida si el texto ingresado es un texto
     if(isNaN(numeroI))
     {
         alert('El valor ingresado no es un número!');
@@ -10,10 +14,12 @@ let procesoFactorial = () =>
     }
     else
     {
+        //Recorrer hasta el número ingresado
         for (let x = 1; x <= numeroI; x++) 
         {
             numeroFactorial *= x;
         }
+        //Resultado que se enviara en el div
         resultadoFinal.textContent = `El factorial del número ${numeroI} es: ${numeroFactorial}`;
     }
 }
